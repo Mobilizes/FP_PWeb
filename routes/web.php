15306e2d15034cd846164d7d80f6bd7656cd825f
Gerry Nicholas <gerrynicholas76@gmail.com>
2024-12-02 12:47:05 +0700

    testing doang ttg routing laravel

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,24 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('homepage');
+});
+
+Route::get('/blade-welcome', function () {
     return view('welcome');
+});
+
+Route::get('/template-dashboard', function () {
+    return view('template-dashboard');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/cart', function () {
+    return view('cart');
+})->middleware(['auth', 'verified'] )->name('cart');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
