d4e7793a4721063c545dcbd6901bd1c4376826da
Gerry Nicholas <gerrynicholas76@gmail.com>
2024-12-03 11:04:09 +0700

    save progres

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
-});
-
-Route::get('/blade-welcome', function () {
     return view('welcome');
 });
 
-Route::get('/template-dashboard', function () {
-    return view('template-dashboard');
+Route::get('/auth/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
 
 Route::get('/dashboard', function () {
